cc72a9a683fe7b47dcb5751d8829814bf54c8bf4
Sidwyn Koh <sidwyn@sidwyns-mbp.lan>
2018-10-21 16:54:38 -0700

    Add button

--- bb-components/nano-address/nano-address.style.js
+++ bb-components/nano-address/nano-address.style.js
@@ -3,5 +3,11 @@ import { addStyles } from 'destyle'
 import theme from '../theme'
 
 addStyles('BB-NanoAddress', {
-  root: css``
+  root: css`
+    display: flex;
+    flex-wrap: wrap;
+  `,
+  address: css`
+    flex-grow: 1;
+  `
 })
