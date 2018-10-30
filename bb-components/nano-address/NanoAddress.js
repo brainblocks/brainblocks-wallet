35fb20b055bb62529a0fd5547c2c33bbd620732e
Sidwyn Koh <sidwyn@sidwyns-mbp.lan>
2018-10-29 20:09:18 -0700

    Add examples and hoverable button

--- bb-components/nano-address/NanoAddress.js
+++ bb-components/nano-address/NanoAddress.js
@@ -1,7 +1,6 @@
-// @flow
-import * as React from 'react'
-
 import Button from '../button/Button'
+// @flow
+import { Component } from 'react'
 import { destyle } from 'destyle'
 
 type Props = {
@@ -9,6 +8,8 @@ type Props = {
   address: string,
   /** Show copy button */
   copyable?: boolean,
+  /** Show copy button on hover */
+  hover?: boolean,
   /** Number of characters before ellipsis */
   startChars: number,
   /** Number of characters after ellipsis */
@@ -20,30 +21,67 @@ type Props = {
 /**
  * NanoAddress.
  */
-export const NanoAddress = ({
-  styles,
-  address,
-  startChars = 10,
-  endChars = 5,
-  copyable = false,
-  ...rest
-}: Props) => {
-  const start = address.substr(0, startChars)
-  const end = address.substr(address.length - endChars)
-  return (
-    <span className={styles.root}>
-      <span className={styles.address}>
-        <span className={styles.start}>{start}</span>
-        <span className={styles.ellipsis}>&hellip;</span>
-        <span className={styles.end}>{end}</span>
+export class NanoAddress extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isHovering: false
+    }
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+
+  static defaultProps = {
+    startChars: 10,
+    endChars: 5,
+    copyable: false,
+    hoverable: false
+  }
+
+  onMouseEnter() {
+    this.setState({
+      isHovering: true
+    })
+  }
+
+  onMouseLeave() {
+    this.setState({
+      isHovering: false
+    })
+  }
+
+  render() {
+    const {
+      styles,
+      address,
+      startChars,
+      endChars,
+      copyable,
+      hoverable,
+      ...rest
+    } = this.props
+    const start = address.substr(0, startChars)
+    const end = address.substr(address.length - endChars)
+
+    return (
+      <span
+        className={styles.root}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <span className={styles.address}>
+          <span className={styles.start}>{start}</span>
+          <span className={styles.ellipsis}>&hellip;</span>
+          <span className={styles.end}>{end}</span>
+        </span>
+        {(copyable || (hoverable && this.state.isHovering)) && (
+          <Button type="secondary" style={{ fontSize: 14 }}>
+            Copy
+          </Button>
+        )}
       </span>
-      {copyable && (
-        <Button type="secondary" style={{ fontSize: 14 }}>
-          Copy
-        </Button>
-      )}
-    </span>
-  )
+    )
+  }
 }
 
 export default destyle(NanoAddress, 'BB-NanoAddress')
--- bb-components/nano-address/NanoAddress.md
+++ bb-components/nano-address/NanoAddress.md
@@ -1,19 +1,49 @@
-Example:
+Simple example:
 
 ```js
 <div>
   <p>
     <NanoAddress address="xrb_1brainb3zz81wmhxndsbrjb94hx3fhr1fyydmg6iresyk76f3k7y7jiazoji" />
   </p>
+</div>
+```
+
+Example with copy button:
+
+```js
+<div>
   <p>
     <NanoAddress
       copyable
       address="xrb_1brainb3zz81wmhxndsbrjb94hx3fhr1fyydmg6iresyk76f3k7y7jiazoji"
     />
   </p>
+</div>
+```
+
+Example with copy button and character length:
+
+```js
+<div>
   <p>
     <NanoAddress
       copyable
+      startChars={20}
+      endChars={8}
+      address="xrb_1brainb3zz81wmhxndsbrjb94hx3fhr1fyydmg6iresyk76f3k7y7jiazoji"
+    />
+  </p>
+</div>
+```
+
+Example with hoverable copy button:
+
+```js
+<div>
+  <p>
+    <NanoAddress
+      style={{ height: '40px' }}
+      hoverable
       startChars={20}
       endChars={8}
       address="xrb_1brainb3zz81wmhxndsbrjb94hx3fhr1fyydmg6iresyk76f3k7y7jiazoji"
