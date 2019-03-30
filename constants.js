a909b141eb147d002e1d088fb46a5be8d7e69d9f
Ty Schenk <tyschenk2@me.com>
2019-03-29 22:13:48 -0400

    cleanup

--- constants.js
+++ constants.js
@@ -1,5 +1,3 @@
-// const config = require('dotenv').config();
-
 export const BASE_API_URL = process.env.BASE_API_URL
 export const PRICE_API_URL = 'https://api.brainblocks.io/api/exchange/rates'
 export const LOCAL_STORAGE_AUTH_TOKEN_KEY = '__bb_auth_token__'
